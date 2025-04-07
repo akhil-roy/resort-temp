13d3a5dfe930ad3d132eec4661da6c2c5fe99264
akhil-roy <akhilchathanattu@gmail.com>
2025-04-08 02:15:57 +0530

    added animations on scroll on index

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,34 @@ document.getElementById('prev').onclick = function(){
 // }
 
 // autoSlide();
+
+// animation on scroll
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.15)) {
+      displayScrollElement(el);
+    }
+  })
+};
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
